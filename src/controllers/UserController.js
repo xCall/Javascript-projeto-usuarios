54074b7d8f98b93b22934ba1a0f680e3db884340
xCall <carlosfaita99@gmail.com>
2020-04-29 23:59:31 -0300

    Editar e validar usuários
    Editar e validar usuarios e dados

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,23 +1,50 @@
 class UserController {
 
-  constructor(formId, tableId, btnSubmit) {
+  constructor(formIdCreate, formIdUpdate, tableId, btnSubmit) {
 
-    this.formEl = document.getElementById(formId)
+    this.formEl = document.getElementById(formIdCreate)
+    this.formUpdateEl = document.getElementById(formIdUpdate)
     this.tableEl = document.getElementById(tableId)
 
     this.btnSubmit = document.querySelector(btnSubmit)
 
     this.onSubmit()
+    this.onEdit()
+
+  }
+
+  onEdit() {
+
+    document.querySelector('#box-user-update .btn-cancel').addEventListener("click", e => {
+
+      this.showPanelCreate()
+      this.lockBtn(false)
+
+    })
+
+    this.formUpdateEl.addEventListener("submit", event => {
+
+      this.lockBtn(true)
+      
+    })
+    
+    
+
+  }
+
+  lockBtn(value) {
+
+    event.preventDefault()
+
+    this.btnSubmit.disabled = value;
 
   }
 
   onSubmit() {
 
     this.formEl.addEventListener('submit', event => {
-
-      event.preventDefault()
-
-      this.btnSubmit.disabled = true;
+      
+      this.lockBtn(true)
 
       let values = this.getValues()
 
@@ -31,7 +58,7 @@ class UserController {
 
         this.formEl.reset()
 
-        this.btnSubmit.disabled = false
+        this.lockBtn(false)
 
       }, (e) => {
 
@@ -154,14 +181,62 @@ class UserController {
       <td>${(dataUser.admin ? 'Sim' : 'Não')}</td>
       <td>${Utils.dateFormat(dataUser.register)}</td>
       <td>
-        <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+        <button type="button" class="btn btn-primary btn-edit btn-xs btn-flat">Editar</button>
         <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
       </td>
     `
+    tr.querySelector('.btn-edit').addEventListener("click", e => {
+
+      let json = JSON.parse(tr.dataset.user)
+      let form = document.querySelector('#form-user-update')
+
+      for (let name in json) {
+
+        let field = form.querySelector("[name=" + name.replace("_", "") + "]")
+        if (field) {
+
+          switch (field.type) {
+
+            case 'file':
+              continue
+              break
+            case 'radio':
+              field = form.querySelector("[name=" + name.replace("_", "") + "][value=" + json[name] + "]")
+              field.checked = true
+              break
+            case 'checkbox':
+              field.checked = json[name]
+              break
+            default:
+              field.value = json[name]
+
+          }
+
+        }
+
+      }
+
+      this.showPanelUpdate()
+
+    })
 
     this.tableEl.appendChild(tr)
 
     this.updateCount()
+
+  }
+
+  showPanelCreate() {
+
+    document.querySelector('#box-user-create').style.display = "block"
+    document.querySelector('#box-user-update').style.display = "none"
+
+  }
+
+  showPanelUpdate() {
+
+    document.querySelector('#box-user-create').style.display = "none"
+    document.querySelector('#box-user-update').style.display = "block"
 
   }
 
@@ -176,7 +251,7 @@ class UserController {
 
       let user = JSON.parse(tr.dataset.user)
 
-      if(user._admin) numberAdmin++
+      if (user._admin) numberAdmin++
 
     })
 
--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,1 @@
-let userController = new UserController('form-user-create', 'table-users', '[type=submit]')
+let userController = new UserController('form-user-create', 'form-user-update', 'table-users', '[type=submit]')
